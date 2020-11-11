e234950f3ebf1ce15f33188456e0c176ec729c6b
PivoKrtek <71064195+PivoKrtek@users.noreply.github.com>
2020-11-11 10:58:54 +0100

    Test doing game of life

---
+++ TheGameOfLife/TheGameOfLife/Cell.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGameOfLife
+{
+    class Cell
+    {
+        public int YPosition { get; set; }
+        public int XPosition { get; set; }
+        public bool Alive { get; set; }
+
+        public Cell(int ypos, int xpos)
+        {
+            YPosition = ypos;
+            XPosition = xpos;
+            Alive = RandomDeadOrAlive();
+        }
+        public Cell(int ypos, int xpos, bool alive)
+        {
+            YPosition = ypos;
+            XPosition = xpos;
+            Alive = alive;
+        }
+
+        private bool RandomDeadOrAlive()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 3);
+            if (number == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static List<Cell> CreateCells()
+        {
+            List<Cell> listOfCells = new List<Cell>();
+            for (int y = 1; y <= Program.YSize; y++)
+            {
+                for (int x = 1; x <= Program.XSize; x++)
+                {
+                    listOfCells.Add(new Cell(y, x));
+                }
+            }
+            return listOfCells;
+        }
+        public static bool CheckIfSquaresAroundIsAlive(int y, int x, Cell cell)
+        {
+            if (y == cell.YPosition && x == cell.XPosition)
+            { return false; }
+            if ((cell.YPosition == y-1 || cell.YPosition == y || cell.YPosition == y+1) && (cell.XPosition == x-1 || cell.XPosition == x || cell.XPosition == x + 1))
+            { return cell.Alive; }
+            return false;
+        }
+        public static bool DecideDeadOrAlive(bool alive, int numberOfAliveSquaresAround)
+        {
+            if (alive)
+            { return StayAlive(numberOfAliveSquaresAround); }
+
+            return BecomeAlive(numberOfAliveSquaresAround);
+        }
+
+        private static bool BecomeAlive(int numberOfAliveSquaresAround)
+        {
+            if (numberOfAliveSquaresAround == 3)
+            { return true; }
+            return false;
+        }
+
+        private static bool StayAlive(int numberOfAliveSquaresAround)
+        {
+            if (numberOfAliveSquaresAround == 2 || numberOfAliveSquaresAround == 3)
+            { return true; }
+            return false;
+        }
+    }
+}
---
+++ TheGameOfLife/TheGameOfLife/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheGameOfLife
+{
+    class Program
+    {
+        public static int YSize { get; set; }
+        public static int XSize { get; set; }
+        public static List<Cell> ListOfCells { get; set; }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            YSize = 26;
+            XSize = 50;
+            ListOfCells = Cell.CreateCells();
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                PrintOutCells();
+                ListOfCells = CheckEachSquareWillStayDeadOrAlive();
+                System.Threading.Thread.Sleep(100);
+                
+
+            }
+
+
+        }
+
+        static void PrintOutCells()
+        {
+            for (int y = 1; y <= Program.YSize; y++)
+            {
+                for (int x = 1; x <= Program.XSize; x++)
+                {
+                    var c1 = ListOfCells
+                        .Where(c => c.XPosition == x && c.YPosition == y)
+                        .Select(c => c.Alive)
+                        .ToList();
+                    if (c1[0])
+                    { Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("\u2588\u2588");
+                        
+                    }
+                    else
+                    { //Console.Write("\u25A1"); 
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static List<Cell> CheckEachSquareWillStayDeadOrAlive()
+        {
+            List<Cell> newListCells = new List<Cell>();
+            for (int y = 1; y <= Program.YSize; y++)
+            {
+                for (int x = 1; x <= Program.XSize; x++)
+                {
+                    var c1 = ListOfCells
+                        .Where(c => c.XPosition == x && c.YPosition == y)
+                        .ToList();
+
+                    var c2 = ListOfCells
+                        .Where(c => Cell.CheckIfSquaresAroundIsAlive(y, x, c))
+                        .ToList();
+
+                    bool alive = Cell.DecideDeadOrAlive(c1[0].Alive, c2.Count());
+
+                    newListCells.Add(new Cell(y, x, alive));
+                }
+            }
+            return newListCells;
+        }
+    }
+}
